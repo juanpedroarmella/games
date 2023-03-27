b202656fb4795cce63ef040e7ed80797053c4134
Juan Pedro Armella <juanpedroarmella@gmail.com>
2023-03-27 18:53:48 -0300

    feat(proyect): init

---
+++ db/insert.sql
@@ -0,0 +1,59 @@
+INSERT INTO
+  games (name, type, price, stock, image, clearance)
+VALUES
+  (
+    'Super Mario Bros',
+    'Plataforma',
+    29,
+    100,
+    'https://source.unsplash.com/random/200x200/?game',
+    0
+  );
+
+INSERT INTO
+  games (name, type, price, stock, image, clearance)
+VALUES
+  (
+    'The Legend of Zelda',
+    'Aventura',
+    49,
+    50,
+    'https://source.unsplash.com/random/200x200/?zelda',
+    1
+  );
+
+INSERT INTO
+  games (name, type, price, stock, image, clearance)
+VALUES
+  (
+    'Grand Theft Auto V',
+    'Acción',
+    59,
+    200,
+    'https://source.unsplash.com/random/200x200/?gta',
+    0
+  );
+
+INSERT INTO
+  games (name, type, price, stock, image, clearance)
+VALUES
+  (
+    'FIFA 22',
+    'Deportes',
+    39,
+    75,
+    'https://source.unsplash.com/random/200x200/?fifa',
+    0
+  );
+
+INSERT INTO
+  games (name, type, price, stock, image, clearance)
+VALUES
+  (
+    'Resident Evil Village',
+    'Horror',
+    69,
+    25,
+    'https://source.unsplash.com/random/200x200/?residentevil',
+    1
+  );
---
+++ src/games.php
@@ -0,0 +1,89 @@
+<?php
+include "config.php";
+include "utils.php";
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+$dbConn =  connect($db);
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET['id']))
+    {
+      $sql = $dbConn->prepare("SELECT * FROM games where id=:id");
+      $sql->bindValue(':id', $_GET['id']);
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+      exit();
+	  }
+    else {
+      $sql = $dbConn->prepare("SELECT * FROM games");
+      $sql->execute();
+      $sql->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll()  );
+      exit();
+	}
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $input = $_POST;
+    $sql = "INSERT INTO games
+          (name, type, price, stock, image, clearance)
+          VALUES
+          (" . getParams($input) . ")";
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+    $statement->execute();
+    $postId = $dbConn->lastInsertId();
+    if($postId)
+    {
+      $input['id'] = $postId;
+      header("HTTP/1.1 200 OK");
+      echo json_encode($input);
+      exit();
+    }
+}
+
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
+{
+	$id = $_GET['id'];
+  $statement = $dbConn->prepare("DELETE FROM games where id=:id");
+  $statement->bindValue(':id', $id);
+  $statement->execute();
+	header("HTTP/1.1 200 OK");
+	exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'PUT')
+{
+    $input = $_GET;
+    $postId = $input['id'];
+    $fields = getParams($input);
+
+    $sql = "
+          UPDATE games
+          SET $fields
+          WHERE id='$postId'
+           ";
+
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+
+    $statement->execute();
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+
+//En caso de que ninguna de las opciones anteriores se haya ejecutado
+header("HTTP/1.1 400 Bad Request");
+
+?>
