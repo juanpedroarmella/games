87ac5d8ff863a25de8eea10252f6fce92ad896c3
Juan Pedro Armella <juanpedroarmella@gmail.com>
2023-03-28 00:46:15 -0300

    fix(env): again

--- src/config.php
+++ src/config.php
@@ -3,11 +3,10 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv = Dotenv::createUnsafeImmutable(dirname(__DIR__));
 $dotenv->load();
 $host = getenv('DB_HOST');
 $dbname = getenv('DB_NAME');
 $user = getenv('DB_USER') ;
 $password = getenv('DB_PASS');
-
 ?>
